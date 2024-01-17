8d6d6c5f34cc4b5d8c9399a0c8d752caee451113
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-01-17 10:47:48 +0000

    specify that the output is in meters

--- src/p7_main/main.lua
+++ src/p7_main/main.lua
@@ -19,9 +19,9 @@
 g_savedata.userAddonConfig = {
     -- do not split "property.blabla" over multiple lines. stormworks parses the text, not the code, so it will not work and crash your game
     asteroidDensity = property.slider("Asteroid Density - How many asteroids to spawn at once", 1.0, 15.0, 1.0, 12.0),
-    despawnDistance = property.slider("Asteroid Despawn Range - How far a player must go from an asteroid for it to despawn", 100.0, 500.0, 1.0, 150.0),
-    spawnDistance = property.slider("Asteroid Spawn Distance - How close/far an asteroid spawns near a player", 12.0, 200.0, 1.0, 65.0),
-    travelDistance = property.slider("Required Travel Distance To Spawn Asteroids - How far a player must travel for asteroids to spawn near them", 25.0, 2000.0, 10.0, 100.0)
+    despawnDistance = property.slider("Asteroid Despawn Range (meters) - How far a player must go from an asteroid for it to despawn", 100.0, 500.0, 1.0, 150.0),
+    spawnDistance = property.slider("Asteroid Spawn Distance (meters) - How close/far an asteroid spawns near a player", 12.0, 200.0, 1.0, 65.0),
+    travelDistance = property.slider("Required Travel Distance To Spawn Asteroids (meters) - How far a player must travel for asteroids to spawn near them", 25.0, 2000.0, 10.0, 100.0)
 }
 
 -------------------------------
