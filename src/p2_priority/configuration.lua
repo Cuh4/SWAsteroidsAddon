ab6e72f1c720104fbc9f61679bf2e54cb893d8c3
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-01-17 12:34:42 +0000

    show required dlcs

--- src/p2_priority/configuration.lua
+++ src/p2_priority/configuration.lua
@@ -28,6 +28,23 @@ addonConfiguration = {
         description = {
             short = "An addon that adds asteroids to space.",
             long = "A performant addon that periodically spawns asteroids around players if they are in space."
+        },
+
+        requiredDLCs = {
+            space = {
+                name = "Space",
+                enabled = server.dlcSpace()
+            },
+
+            arid = {
+                name = "Industrial Frontier",
+                enabled = server.dlcArid()
+            },
+
+            weapons = {
+                name = "Weapons",
+                enabled = server.dlcWeapons()
+            }
         }
     }
 }
--- src/p6_eventlisteners/player_join/infoMessage.lua
+++ src/p6_eventlisteners/player_join/infoMessage.lua
@@ -20,13 +20,31 @@
 ---@param player af_services_player_player
 AuroraFramework.services.playerService.events.onJoin:connect(function(player)
     AuroraFramework.services.timerService.delay.create(1, function() -- wait a little because messages don't show if they are sent right when a player joins
+        -- Format DLCs
+        DLCsFormatted = {}
+        missingDLCsCount = 0
+
+        for _, dlc in pairs(addonConfiguration.information.requiredDLCs) do
+            if not dlc.enabled then
+                missingDLCsCount = missingDLCsCount + 1
+            end
+
+            table.insert(
+                DLCsFormatted,
+                ("[%s | %s]"):format(dlc.name, dlc.enabled and "Enabled" or "Disabled")
+            )
+        end
+
         -- Send credit message
         helpersLibrary.chat.send(
             table.concat({
                 ("This server has %s enabled."):format(addonConfiguration.information.addonName),
                 addonConfiguration.information.description.long,
                 "---",
-                addonConfiguration.credit.discordServerInvite..(" (@%s on Discord)"):format(addonConfiguration.credit.discordUsername)
+                addonConfiguration.credit.discordServerInvite..(" (@%s on Discord)"):format(addonConfiguration.credit.discordUsername),
+                "---",
+                ("Required DLCs: %s (%s missing)"):format(table.concat(DLCsFormatted, ", "), missingDLCsCount),
+                missingDLCsCount > 0 and "[!!] Please create a new save with the required DLCs enabled." or nil
             }, "\n"),
 
             player
